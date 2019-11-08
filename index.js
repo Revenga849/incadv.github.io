da2ecf6a363b32c753d03be880a582654f60fb3d
AChistyakov <sanchez849@Inbox.ru>
2019-11-08 12:12:18 +0300

    Branched calculator version for upcoming patch

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ $(function () {
   $('[data-toggle="dropdown"]').dropdown();
 })
 
+function rpprInc() {
+	var rppr = $('#rppr')[0];
+	rppr.value = Number.parseInt(rppr.value) + 1;
+}
+
+function rpprDec() {
+	var rppr = $('#rppr')[0];
+	var rpprValue = Math.max(Number.parseInt(rppr.value)-1,0);
+	rppr.value = rpprValue;
+	
+}
+
+function rpdrInc() {
+	var rpdr = $('#rpdr')[0];
+	rpdr.value = Number.parseInt(rpdr.value) + 1;
+}
+
+function rpdrDec() {
+	var rpdr = $('#rpdr')[0];
+	var rpdrValue = Math.max(Number.parseInt(rpdr.value)-1,0);
+	rpdr.value = rpdrValue;
+	
+}
+
 var formatValue = function (value) {
 	if (value.layer instanceof Decimal || value.layer > 2) {
 		return value.toString();
@@ -31,6 +55,16 @@ var formatValue = function (value) {
 
 
 /* PRESTIGE */
+// RPPR - number of "Reduce prestige points requirements" ascension upgrades
+// RPDR - number of "Reduce prestige diminishing return" ascension upgrades
+// LM = 100 * MAX(layer - RPPR, 1)^2
+// LP = 0.8^( 0.98^(RPDR * sqrt(layer - RPPR)) )
+// pcl - points needed
+// points = sqrt((pcl / LM)^LP)
+
+// Reverse:
+// pcl = LM * points^(2 * (1 / LP)^sqrt(layer - RPPR))
+// pcl = LM * points^(2 * LP^(-sqrt(layer - RPPR)))
 
 function estimatePointsForLayer(curLayer, targetLayer, target) {
 	if (targetLayer < 1) {
@@ -38,7 +72,17 @@ function estimatePointsForLayer(curLayer, targetLayer, target) {
 	}
 	points[targetLayer] = target;
 	var layer = new Decimal(targetLayer-1);
-	var pointsNeeded = new Decimal(100).mul(layer.sqr()).mul(target.pow(new Decimal(2).mul(new Decimal(1.25).pow(layer.sqrt()))));
+	var diminishingReturn = new Decimal(0.8);
+	if ($('#rppr')[0].value != "0") {
+		layer = layer.minus($('#rppr')[0].value);
+	}
+	var layermult = Decimal.max(new Decimal(1), layer).sqr().mul(new Decimal(100));
+	var layerpow = new Decimal(0.8);
+	if ($('#rpdr')[0].value != "0") {
+		layerpow = layerpow.pow(new Decimal(0.98).pow($('#rpdr')[0].value));
+	}
+	//var pointsNeeded = layermult.mul(target.pow(new Decimal(2).mul(layerpow.recip().pow(layer.sqrt()))));
+	var pointsNeeded = target.pow(layerpow.pow(layer.sqrt().neg()).mul(new Decimal(2))).mul(layermult);
 	
 	if (curLayer == targetLayer) {
 		return pointsNeeded;
@@ -71,7 +115,7 @@ function calculate() {
 				color = '0' + color;
 			}
 			var colorcode = '#ff' + color + color;
-			console.log(i + " of " + points.length + " " + colorcode);
+			//console.log(i + " of " + points.length + " " + colorcode);
 			if (first) {
 				first = false;
 				var layerstr = "In <span style='font-weight:bold;color:" + colorcode + ";'>Layer " + b + "</span> you need: " + formatValue(a);
@@ -89,110 +133,32 @@ function calculate() {
 
 
 /* ASCENSION */
+// points = (log(log(HL)) * (PL-10) / 100)^1.6 * 100
+// pcl = unspent+stats^2.2
+// points = (points / 100 * (pcl+1)^0.125)^0.8 * 100
 
-function getAvailableAscensionPoints(){
-    var availableAscensionPoints = [];
-    if(typeof(game.highestAscensionLevel) == 'undefined') return availableAscensionPoints;
-    if(game.highestAscensionLevel.log10().cmp(10) < 0 || game.highestAscensionLevel.log10().log10() < 1){
-        var percentageToNextAscension = new Decimal(0);
-    } else {
-        // points = (log(log(HL)) * (PL-10) / 100)^1.45 * 100
-        var prestigeLayersUnlocked = (typeof(game.prestige) !== 'undefined' ? game.prestige.length : 1);
-        if(typeof(availablePrestigePoints) !== 'undefined' && availablePrestigePoints.length > prestigeLayersUnlocked)
-            prestigeLayersUnlocked = availablePrestigePoints.length;
-        var percentageToNextAscension = game.highestAscensionLevel.log10().log10().times(prestigeLayersUnlocked > 10 ? prestigeLayersUnlocked-10 : 0).dividedBy(100).pow(1.45).times(100).floor();
-    }
-    ascensionPointsFromCurrentLayer = new Decimal(0);
-	// pcl = unspent+stats^2.2
-    if(typeof(game.ascension) !== 'undefined' && typeof(game.ascension[0]) !== 'undefined') {
-        if(game.ascension[0][0].cmp(0) > 0)
-            ascensionPointsFromCurrentLayer = ascensionPointsFromCurrentLayer.plus(game.ascension[0][0]);
-        for(let i2 = 1; i2 <= 1; i2++){
-            if(game.ascension[0][i2].cmp(1) > 0){
-                ascensionPointsFromCurrentLayer = ascensionPointsFromCurrentLayer.plus(getAscensionPointsFromStatUpgrades(game.ascension[0][i2]));
-            }
-        }
-    }
-    if(percentageToNextAscension.cmp(100) > 0){
-		// points = (points / 100 * (pcl+1)^0.125)^0.8 * 100
-        percentageToNextAscension = percentageToNextAscension.dividedBy(100).times(ascensionPointsFromCurrentLayer.plus(1).sqrt().sqrt().sqrt()).pow(0.8).times(100).floor();
-        if(percentageToNextAscension.cmp(10000) > 0)
-            percentageToNextAscension = percentageToNextAscension.minus(10000).times(new Decimal(0.9).pow(percentageToNextAscension.log10().minus(4))).plus(10000).floor();
-    }
-    availableAscensionPoints[0] = percentageToNextAscension.dividedBy(100).floor();
-    var nextAscensionAmount = percentageToNextAscension.dividedBy(100).plus(1).floor();
-    var nextAscensionLayer = 1;
-    percentageToNextAscension = modulo(percentageToNextAscension, 100);
-    for(let i = 1; i < Number.MAX_SAFE_INTEGER; i++){
-        if(typeof(game.ascension) == 'undefined' || typeof(game.ascension[i - 1]) == 'undefined') break;
-        ascensionPointsFromPreviousLayer = availableAscensionPoints[i - 1];
-        ascensionPointsFromPreviousLayer = ascensionPointsFromPreviousLayer.plus(game.ascension[i - 1][0]);
-        if(game.ascension[i - 1][1].cmp(1) > 0){
-            ascensionPointsFromPreviousLayer = ascensionPointsFromPreviousLayer.plus(getAscensionPointsFromStatUpgrades(game.ascension[i - 1][1]));
-        }
-        ascensionPointsFromCurrentLayer = new Decimal(0);
-        if(typeof(game.ascension) !== 'undefined' && typeof(game.ascension[i]) !== 'undefined') {
-            if(game.ascension[i][0].cmp(0) > 0)
-                ascensionPointsFromCurrentLayer = ascensionPointsFromCurrentLayer.plus(game.ascension[i][0]);
-            if(game.ascension[i][1].cmp(1) > 0){
-                ascensionPointsFromCurrentLayer = ascensionPointsFromCurrentLayer.plus(getAscensionPointsFromStatUpgrades(game.ascension[i][1]));
-            }
-        }
-
-        let innerPercentageToNextAscension = ascensionPointsFromPreviousLayer.dividedBy(new Decimal(i).pow(2)).floor();
-        if(innerPercentageToNextAscension.cmp(100) >= 1) {
-            innerPercentageToNextAscension = innerPercentageToNextAscension.dividedBy(100).times(ascensionPointsFromCurrentLayer.plus(1).sqrt().sqrt().sqrt()).pow(new Decimal(0.8).pow(new Decimal(i).sqrt())).times(100).floor();
-            if(innerPercentageToNextAscension.cmp(10000) > 1)
-                innerPercentageToNextAscension = innerPercentageToNextAscension.minus(10000).times(new Decimal(0.9).pow(innerPercentageToNextAscension.log10().minus(4))).plus(10000).floor();
-            if(innerPercentageToNextAscension.dividedBy(100).cmp(ascensionPointsFromCurrentLayer) > 0)
-                innerPercentageToNextAscension = innerPercentageToNextAscension.dividedBy(100).minus(ascensionPointsFromCurrentLayer).dividedBy(innerPercentageToNextAscension.dividedBy(100).minus(ascensionPointsFromCurrentLayer).sqrt()).plus(ascensionPointsFromCurrentLayer).times(100).floor();
-        }
-        if(innerPercentageToNextAscension.cmp(50) >= 0){
-            percentageToNextAscension = modulo(innerPercentageToNextAscension, 100);
-            nextAscensionAmount = innerPercentageToNextAscension.dividedBy(100).plus(1).floor();
-            nextAscensionLayer = i+1;
-        }
-        availableAscensionPointsForLayer = innerPercentageToNextAscension.dividedBy(100).floor();
-        if(availableAscensionPointsForLayer > 0 || typeof(game.ascension[i]) !== 'undefined')
-        availableAscensionPoints[i] = availableAscensionPointsForLayer;
-        else
-            break;
-    }
-    if(percentageToNextAscension.cmp(20) >= 0 || nextAscensionAmount.cmp(1) >= 1)
-        document.getElementById("next-ascension-percentage-text").innerHTML = " - Progress to Ascension" + (nextAscensionLayer > 1 ? " layer " + nextAscensionLayer : "") + (nextAscensionAmount.cmp(1) >= 1 ? " point " + renderNumber(nextAscensionAmount) : "") + ": " + renderNumber(percentageToNextAscension) + "%";
-    else
-        document.getElementById("next-ascension-percentage-text").innerHTML = "";
-    return availableAscensionPoints;
-}
+// Reverse:
+// 
+// HL = points^1.25 / (pcl+1)^0.125
 
 function estimateAscPoints(curPoints) {
-	for (let i=1; i<11; i++) {
+	for (let i=1; i<21; i++) {
 		var ascPointsTarget = new Decimal(i);
 		var ascPoints = new Decimal(0);
 		
 		var layer = new Decimal(17);
 		var level = new Decimal('e1e9');
-		while (ascPoints.lt(ascPointsTarget) && level.lt(new Decimal('e1e40'))) {
-			level = level.pow(1.5);
-			//console.log('level: ' + formatValue(level));
-			if (new Decimal('e1e9').pow(new Decimal(5.8).pow(layer.minus(16))).lte(level)) {
-				layer = layer.add(1);
-				//console.log('layer: ' + layer.toNumber());
-			}
-			var ascPointsPercentage = level.log10().log10().times(layer.minus(10)).dividedBy(100).pow(1.45).times(100).floor();
-			//console.log('precentage: ' + formatValue(ascPointsPercentage));
-			if (ascPointsPercentage.gte(100)) {
-				ascPointsPercentage = ascPointsPercentage.dividedBy(100).times(curPoints.plus(1).sqrt().sqrt().sqrt()).pow(0.8).times(100).floor();
-				//console.log('precentage: ' + formatValue(ascPointsPercentage));
-				if(ascPointsPercentage.cmp(10000) > 0) {
-					ascPointsPercentage = ascPointsPercentage.minus(10000).times(new Decimal(0.9).pow(ascPoints.log10().minus(4))).plus(10000).floor();
-					//console.log('precentage: ' + formatValue(ascPointsPercentage));
-				}
-			}
-			ascPoints = ascPointsPercentage.div(100).floor();
-			//console.log('points: ' + formatValue(ascPoints));
+		var ascLevel = ascPointsTarget.pow(1.25);
+		if (ascPointsTarget.gt(1)) {
+			ascLevel = ascLevel.div(curPoints.add(1).pow(0.125)).mul(100).ceil().div(100);
 		}
-		ascText[i] = '<b>' + i + '</b>' + ' Ascension points at layer <b>' + (layer.toNumber()) + '</b>, highest level <b>' + formatValue(level) + '</b>';
+		var ascLayerLevel = Decimal.layeradd(ascLevel.pow(1/1.6).mul(100).div(layer.minus(10)),2);
+		while (ascLayerLevel.gte(level)) {
+			layer = layer.add(1);
+			ascLayerLevel = Decimal.layeradd(ascLevel.pow(1/1.6).mul(100).div(layer.minus(10)),2);
+			level = level.pow(7);
+		}
+		ascText[i] = (i<10?'&nbsp;&nbsp;':'') + '<b>' + i + '</b>' + ' Ascension points at layer <b>' + (layer.toNumber()) + '</b>, highest level <b>' + formatValue(ascLayerLevel.pow(1.01)) + '</b>';
 	}
 }
 
@@ -227,11 +193,9 @@ function calculateAscension() {
 				color = '0' + color;
 			}
 			var colorcode = '#ff' + color + color;
-			//console.log(i + " of " + ascText.length + " " + colorcode);
 			var li = $('<li />').html(a).addClass('point-' + b);
 			$('#calculation > ol').append(li);
 		});
-		//$('#calculation > ol')[0].lastChild.lastChild.style.fontWeight = 'bold';
 	} else {
 		document.getElementById('tabcontent').style.display = "none";
 	}
